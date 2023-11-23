e260eb554928382de696eb4b6d242881a572e3cc
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 10:25:26 +0100

    Header content generates now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
+import { Link, useNavigate } from "react-router-dom"
+
+function HeaderItem ({name, url}) {
+  const navigate = useNavigate()
+  return (
+      <button onClick={() => navigate(url)}>
+        {name}
+      </button>
+  )
+}
+
 export default function Header () {
   return (
     <header>
       <h1>Rebecca Noy</h1>
+      <nav>
+        <HeaderItem name="Home" url="/"/>
+        <HeaderItem name="Projects" url="/projects"/>
+        <HeaderItem name="About Me" url="/about-me"/>
+        <HeaderItem name="Imprint" url="/imprint"/>
+      </nav>
     </header>
   )
 }
