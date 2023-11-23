02733ed32a8d466749515bae3234215a593bc6b7
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 11:16:17 +0100

    Work on About Page css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,11 @@
+import profile  from "../assets/LJStuffTransparent.png"
+
 export default function About () {
   return (
     <div>
+      <div className="imgContainer profile">
+        <img src={profile} alt="A graphic illustration in comic-like style, done by Lija J, contact: lijaj.art@gmail.com" />
+      </div>
       ABOUT ME
     </div>
   )
