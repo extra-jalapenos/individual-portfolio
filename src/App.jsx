70bb85ce3adc465e21a7ac6c5eb84c2f01a72515
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 09:53:41 +0100

    Added Home and placeholder text.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,14 +9,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <Home />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home () {
+  return (
+    <main>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, officiis vero. Natus aliquam ut, dolor, eveniet dolorem ipsam est facilis similique vel et placeat culpa delectus eum officia reprehenderit magni?
+    </main>
+  )
+}
