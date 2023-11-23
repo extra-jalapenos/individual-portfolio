13b917c571904aa99fbb8d263b1e092e12ef940f
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 11:26:26 +0100

    CSS for general page layout and AboutPage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,15 @@ import profile  from "../assets/LJStuffTransparent.png"
 
 export default function About () {
   return (
-    <div>
+    <main className="about">
       <div className="imgContainer profile">
         <img src={profile} alt="A graphic illustration in comic-like style, done by Lija J, contact: lijaj.art@gmail.com" />
       </div>
-      ABOUT ME
-    </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quasi excepturi, eum ratione distinctio accusamus itaque illum nostrum error delectus nesciunt. Voluptatibus vitae possimus culpa dicta minus quia reprehenderit deleniti?
+        </p>
+      </div>
+    </main>
   )
 }
