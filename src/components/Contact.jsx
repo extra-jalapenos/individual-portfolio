6084a2977f88accd7a7c8580767acaa5b4b1700d
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 15:46:24 +0100

    work on form's css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,17 +3,38 @@ import "../css/Form.css"
 import Button from "./Button"
 
 function FormField ({name, description, placeholder, type, handleInput}) {
-  return (
-    <>
-      <label>{description}</label>
-      <input
-        name={name}
-        placeholder={placeholder} 
-        type={type} 
-        onChange={(event) => handleInput(event)}
-      />
-    </>
-  )
+  console.log(name, type)
+  switch (type) {
+    case "text":
+      return (
+        <>
+          <label>{description}</label>
+          <input
+            name={name}
+            placeholder={placeholder} 
+            type={type} 
+            onChange={(event) => handleInput(event)}
+          />
+        </>
+      )
+      break;
+    case "textarea":
+      return (
+        <>
+          <label>{description}</label>
+          <textarea
+            name={name}
+            placeholder={placeholder} 
+            type={type}
+            rows={8}
+            onChange={(event) => handleInput(event)}
+          />
+        </>
+      )
+
+  }
+
+  
 }
 
 export default function ContactForm () {
@@ -34,6 +55,15 @@ export default function ContactForm () {
     console.log(form)
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("Submit")
+  }
+
+  const handleReset = (event) => {
+    form.reset()
+  }
+
   const formFields = [
     {
       name: "name",
@@ -45,13 +75,13 @@ export default function ContactForm () {
       name: "email",
       description: "Your E-Mail",
       placeholder: "you@yourdomain.com",
-      type: "email"
+      type: "text"
     },
     {
       name: "phone",
       description: "Your Phone Number",
       placeholder: "+49 123 4567890",
-      type: "email"
+      type: "text"
     },
     {
       name: "enquiry",
@@ -64,8 +94,11 @@ export default function ContactForm () {
   return (
     <main>
       <form onSubmit={() => console.log(form)}>
-        {formFields.map((field, index) => <FormField key={index} name={field.name} description={field.description} placeholder={field.placeholder} handleInput={handleInput}/>)}
-        <Button name={"Submit"}/>
+        {formFields.map((field, index) => <FormField key={index} name={field.name} type={field.type} description={field.description} placeholder={field.placeholder} handleInput={handleInput}/>)}
+        <div>
+          <button onClick={handleSubmit}>Submit</button>
+          <button onClick={handleReset}>Clear</button>
+        </div>
       </form>
     </main>
   )
