e4665805e1ba6348c5d345b5cd98257b9eeca355
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 11:51:30 +0100

    added imprint in german

--- src/components/Imprint.jsx
+++ src/components/Imprint.jsx
@@ -1,7 +1,18 @@
 export default function Imprint () {
   return (
     <div className="imprint">
-      IMPRINT
+      <h3>Website</h3>
+      Dieses Impressum gilt für alle Angebote auf der Domain –––– inklusive Subdomains (Unterseiten).
+      <h3>Soziale Medien</h3>
+      <p>Dieses Impressum gilt auch für folgende Accounts der sozialen Medien:</p>
+      <p>Instagram: @alexandriadouillette<br/>
+      LinkedIn: rebeccanoy</p>
+      <h3>Angaben gemäß § 5 TMG</h3>
+      <p>Rebecca Noy<br/>
+      Brentanostraße 5<br/>
+      93051 Regensburg</p>
+      <p>Telefon: +4915142337656<br/>
+      E-Mail: rebecca.noy@gmx.de</p>
     </div>
   )
 }
