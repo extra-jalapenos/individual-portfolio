44a25590b3dc0ff5123c879205378b491b8103b8
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 12:57:14 +0100

    filled homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,19 @@
 import { useNavigate } from "react-router-dom"
+import Button from "./Button"
 
 export default function Home () {
-  
+  const navigate = useNavigate()
   return (
     <main>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, officiis vero. Natus aliquam ut, dolor, eveniet dolorem ipsam est facilis similique vel et placeat culpa delectus eum officia reprehenderit magni?
-      <button>
-        Contact
-      </button>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, officiis vero. Natus aliquam ut, dolor, eveniet dolorem ipsam est facilis similique vel et placeat culpa delectus eum officia reprehenderit magni?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, officiis vero. Natus aliquam ut, dolor, eveniet dolorem ipsam est facilis similique vel et placeat culpa delectus eum officia reprehenderit magni?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, officiis vero. Natus aliquam ut, dolor, eveniet dolorem ipsam est facilis similique vel et placeat culpa delectus eum officia reprehenderit magni?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, officiis vero. Natus aliquam ut, dolor, eveniet dolorem ipsam est facilis similique vel et placeat culpa delectus eum officia reprehenderit magni?</p>
+      <div>
+        <Button name={"Contact me"} url={"/contact"} />
+        <Button name={"E-Mail"} url={""} />
+        <Button name={"Looking to hire?"} url={""} />
+      </div>
     </main>
   )
 }
