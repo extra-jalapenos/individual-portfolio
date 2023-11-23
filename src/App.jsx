01b4d47b7125e261f52721f427422f3f6cd73a27
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-23 09:50:05 +0100

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="container">
-      <h1>Rebecca Noy</h1>
+      <Header />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
