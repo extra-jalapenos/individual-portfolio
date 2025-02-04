a5249b1ae85b3b0ea9b52968fed2d8ca7ba425c2
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-02-04 21:05:24 +0100

    add more projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ function ProjectCard ({props}) {
     <div className="projectCard">
       <h2>{title}</h2>
       {description}<br/>
-      <GitHubLink url={link}/>
+      {link && <GitHubLink url={link}/>}
     </div>
   )
 
@@ -42,20 +42,37 @@ export default function Projects () {
 
   const apprendioFrontend = {
     title: "Flashcard / Study App – Frontend",
-    description: `Learn anywhere, anytime, as much (or as little) as your daily routine allows you to with apprendio.online.
-    Technologies used: React, JavaScript, CSS, Html`,
-    link: "AllyDouillette/apprendio-flashcard-app-frontend"
+    description: `Learn anywhere, anytime, as much (or as little) as your daily routine allows you to with apprendio.online.\nTechnologies used: React, JavaScript, CSS, Html`,
+    link: "extra-jalapenos/apprendio-flashcard-app-frontend"
   }
 
   const apprendioBackend = {
     title: "Flashcard / Study App – Backend",
     description: `Technologies used: Express, Prisma ORM, PostgreSQL`,
-    link: "AllyDouillette/apprendio-flashcard-app-backend"
+    link: "extra-jalapenos/apprendio-flashcard-app-backend"
+  }
+
+  const alliWebsite = {
+    title: "Redesign of the alli-app.com website",
+    description: "Non-profit work: maintenance of the website for an app that's directed at people experiencing a pregnancy loss or unwanted pregnancy and go through a chemically induced abortion.",
+  }
+
+  const gymTracker = {
+    title: "Gym Tracker",
+    description: `Ever been member of a gym and dreaded the New Year's seasonal spike? To analyze my gym's data, I built an app to check the occupancy every five minutes and log it into a PostgreSQL database.\nTechnologies used: JavaScript, Prisma ORM, CRON-jobs to trigger`
+  }
+
+  const kieferando = {
+    title: "Kieferando: K.'s personal Lieferando – Frontend",
+    description: "Most of my personal work is tied to my personal life; when my friend had a baby, a mutual friend and I decided to gift her the next-best thing in the world: food. To support her on her journey, we meal-prepped food; as the prepping and delivery involves a bit of coordination, we built an app to manage it.\nTechnologies used: TypeScript, Strapi"
   }
 
   const projects = [
     apprendioFrontend,
-    apprendioBackend
+    apprendioBackend,
+    alliWebsite,
+    kieferando,
+    gymTracker
   ]
 
   return (
