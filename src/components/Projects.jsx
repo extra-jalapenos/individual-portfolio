c48faa07969069b9a593834d68453971f956da88
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-03-15 23:30:09 +0100

    add current state of projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,26 @@
 import { GitHubFontLogo, GithubLogo } from "../assets/github-mark-white"
 
-function GitHubLink ({url}) {
+function GitHubLink ({urls}) {
   return (
     <buttoncontainer>
       <GithubLogo />
       <GitHubFontLogo />
-      <a href={`https://github.com/${url}`}>{url}</a>
+      <div id="links">
+        {urls.map(url => <p><a href={`https://github.com/${url}`}>{url}</a></p>)}
+      </div>
     </buttoncontainer>
   )
 }
 
 function ProjectCard ({props}) {
-  const {title, description, img, link} = props
+  const {title, description, technologies, img, links = []} = props
 
   if (!img) return (
     <div className="projectCard">
       <h2>{title}</h2>
-      {description}<br/>
-      {link && <GitHubLink url={link}/>}
+      <p><b>Technologies</b> {technologies}</p>
+      <p>{description}</p>
+      {links.length > 0 && <GitHubLink urls={links}/>}
     </div>
   )
 
@@ -40,38 +43,36 @@ export default function Projects () {
   //   description: `This API provides information on Taylor Swift's albums and songs. Available endpoints include: album details | song details including appearances | different editions of an album | song lyrics`,
   // }
 
-  const apprendioFrontend = {
-    title: "Flashcard / Study App – Frontend",
-    description: `Learn anywhere, anytime, as much (or as little) as your daily routine allows you to with apprendio.online.\nTechnologies used: React, JavaScript, CSS, Html`,
-    link: "extra-jalapenos/apprendio-flashcard-app-frontend"
-  }
-
-  const apprendioBackend = {
-    title: "Flashcard / Study App – Backend",
-    description: `Technologies used: Express, Prisma ORM, PostgreSQL`,
-    link: "extra-jalapenos/apprendio-flashcard-app-backend"
+  const apprendio = {
+    title: "Flashcard / Study App",
+    description: "Learn anywhere, anytime, as much (or as little) as your daily routine allows you to with apprendio.online.",
+    technologies: "React, JavaScript, CSS, Html, Express, Prisma ORM, PostgreSQL",
+    links: ["extra-jalapenos/apprendio-flashcard-app-frontend", "extra-jalapenos/apprendio-flashcard-app-backend"]
   }
 
   const alliWebsite = {
     title: "Redesign of the alli-app.com website",
     description: "Non-profit work: maintenance of the website for an app that's directed at people experiencing a pregnancy loss or unwanted pregnancy and go through a chemically induced abortion.",
+    technologies: "React, SCSS, Html",
+    links: ["dinosled/alli-website"]
   }
 
   const gymTracker = {
     title: "Gym Tracker",
-    description: `Ever been member of a gym and dreaded the New Year's seasonal spike? To analyze my gym's data, I built an app to check the occupancy every five minutes and log it into a PostgreSQL database.\nTechnologies used: JavaScript, Prisma ORM, CRON-jobs to trigger`
+    description: `Ever been member of a gym and dreaded the New Year's seasonal spike? To analyze my gym's data, I built an app to check the occupancy every five minutes and log it into a PostgreSQL database.`,
+    technologies: "JavaScript, Prisma ORM, CRON-jobs to trigger"
   }
 
-  const kieferando = {
-    title: "Kieferando: K.'s personal Lieferando – Frontend",
-    description: "Most of my personal work is tied to my personal life; when my friend had a baby, a mutual friend and I decided to gift her the next-best thing in the world: food. To support her on her journey, we meal-prepped food; as the prepping and delivery involves a bit of coordination, we built an app to manage it.\nTechnologies used: TypeScript, Strapi"
+  const galleymates = {
+    title: "Galley Mates",
+    description: "Most of my personal work is tied to my personal life; when my friend had a baby, a mutual friend and I decided to gift her the next-best thing in the world: food. To support her on her journey, we meal-prepped food; as the prepping and delivery involves a bit of coordination, we built an app to manage it.",
+    technologies: "TypeScript, Vue3, SCSS, Express, Prisma ORM, PostgreSQL"
   }
 
   const projects = [
-    apprendioFrontend,
-    apprendioBackend,
+    apprendio,
     alliWebsite,
-    kieferando,
+    galleymates,
     gymTracker
   ]
 
